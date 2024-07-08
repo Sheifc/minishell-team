af2e5163235497af9d95c7f0a3da4dd8ca542023
SFC <115345487+Sheifc@users.noreply.github.com>
2024-07-08 09:22:20 +0200

    Update heredoc.c

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -22,13 +22,13 @@ void	save_heredoc(t_cmd *cmd, t_token **tok, t_env *env)
 	i = 1;
 	cmd->fdin = open("hdoc.tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (cmd->fdin == -1)
-		printf("Error making HEREDOC\n");
+		printf("Error making HEREDOC\n"); // ocurre un error con la funcion open, debe generarse un error, no un printf, se tiene que usar la funcion perror
 	while (1)
 	{
 		line = readline("> ");
 		if (line == NULL)
 		{
-			printf("bash: warning: here-document at line %d delimited by (wanted «%s»)\n", i, (*tok)->content);
+			printf("bash: warning: here-document at line %d delimited by (wanted «%s»)\n", i, (*tok)->content); // ocurre un error con la funcion readline, debe generarse un error, no un printf, se tiene que usar la funcion perror
 			break ;
 		}
 		i++;
